6cd0ea8ad8034140b81c7f1b56e89a9cf34f19f2
riajul-alam <riajulalam5@gmail.com>
2024-02-01 00:22:55 +0600

    working with account information

---
+++ src/Account.java
@@ -0,0 +1,34 @@
+package src;
+
+import java.util.Date;
+class Account {
+    private String firstName;
+    private String lastName;
+    private final int accountNumber;
+    private String accountType;
+    private final Date creationDate;
+    private double balance;
+    Account(String firstName,String lastName, int accountNumber, String accountType, double balance) {
+        this.firstName = firstName;
+        this.lastName=lastName;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        creationDate=new Date();
+        this.balance = balance;
+    }
+    public String getAccountName() {
+        return firstName+" "+lastName;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public String getAccountType() {
+        return accountType;
+    }
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    public double getBalance() {
+        return balance;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
-
-class Main{
+package src;
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        String firstName = input.next();
+        System.out.print("Enter last name: ");
+        String lastName = input.next();
+        System.out.print("Enter account number: ");
+        int accountNumber = input.nextInt();
+        System.out.print("Enter account type: ");
+        String accountType = input.next();
+        System.out.print("Enter opening balance: ");
+        double balance = input.nextDouble();
+        Account account= new Account(firstName,lastName, accountNumber, accountType, balance);
+        System.out.println("Account created successfully!");
 
+        //Display Account
+        System.out.print("Account name: ");
+        System.out.println(account.getAccountName());
+        System.out.print("Account number: ");
+        System.out.println(account.getAccountNumber());
+        System.out.print("Account type: ");
+        System.out.println(account.getAccountType());
+        System.out.print("balance: ");
+        System.out.println(account.getBalance());
+        System.out.print("Creation Date: ");
+        System.out.println(account.getCreationDate());
     }
 }
